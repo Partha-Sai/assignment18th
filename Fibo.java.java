18725c8d4c99763be51687e5bdb131549a7d659c
Partha Sai <parthasai.s@pravyuha.com>
2023-07-22 15:18:55 +0530

    Fibonaccai Series Function Added

---
+++ Fibo.java.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Fibo {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
